3af63c29a0908014b4886a254c3e47a5ff9b36e5
imyoyo <imyoyo.suhwan@gmail.com>
2014-06-11 01:26:31 +0900

    add ghost agent

--- server/app/Global.java
+++ server/app/Global.java
@@ -1,13 +1,28 @@
+import com.fasterxml.jackson.databind.ser.impl.PropertySerializerMap;
 import play.*;
 import play.Application;
+import receiver.DAO;
 import receiver.RawDataUdpServer;
-import receiver.UTF8StringReceivingUdpServer;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Global extends GlobalSettings {
 
     @Override
     public void onStart(Application app) {
+        DAO.insertGroupIfNotExist(DAO.TEST_GROUP);
         RawDataUdpServer.start();
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(new Runnable() {
+            Random random = new Random();
+            @Override
+            public void run() {
+                double cpu = random.nextDouble();
+                DAO.insertRaw(DAO.TEST_GROUP, "random-ghost", System.currentTimeMillis(), cpu);
+            }
+        }, 1, 1, TimeUnit.SECONDS);
     }
 
     @Override
